a10ccb42a7a3682bcccb697a24c1b4cdf9ade1dc
FX Coudert <1980544+fxcoudert@users.noreply.github.com>
2026-09-13 11:15:28 +0000

    pake: update 3.16.3 bottle.

--- Formula/p/pake.rb
+++ Formula/p/pake.rb
@@ -6,11 +6,11 @@ class Pake < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "c24c1a3e455d29188a545470fb725b2553158584428bcfae9167bb630456d032"
-    sha256 cellar: :any, arm64_tahoe:       "0936ece7bad7763a88c25d2dacde3c7033e9f73dd07db89773f90d2d1b6362df"
-    sha256 cellar: :any, arm64_sequoia:     "2a9cf3f989869dd71a8c2a1bde27b061bdc5b7bb6a66278d23c4e175a89253d8"
-    sha256 cellar: :any, arm64_linux:       "5285736630172c08c7fbf76b400c8ab23fb09b1dc36da4ead69b6b0a3c8a0c68"
-    sha256 cellar: :any, x86_64_linux:      "7905555bbeed0b5a399b90c327c00c02b9e70f0654aabee46712d7b58ee7ba29"
+    sha256 cellar: :any, arm64_golden_gate: "72d7148d5a1008af858ae4664e7ef30eeb0affb6684da4cf7e9508926d9d26d1"
+    sha256 cellar: :any, arm64_tahoe:       "466ed37a5259e41973af9db605e434b8b7efaf4e5ee8dc11af03a9e49f96c718"
+    sha256 cellar: :any, arm64_sequoia:     "c802e280167223d268c9f4d2b78dd1b2f5dfc8e86bca0cf13653c8ae8962f435"
+    sha256 cellar: :any, arm64_linux:       "9617f0e8b500a6c88f37d607ba44ff5a668fcf324aad4047eece7b081ba7c2ad"
+    sha256 cellar: :any, x86_64_linux:      "2fc223e2323c173c2b8b8c43b148c4313739b6b5b5f91e28674fc4f88b1e9503"
   end
 
   depends_on "pkgconf" => :build
